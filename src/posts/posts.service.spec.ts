7bad9a28563bdd28f67a089c2024c30fd389c482
yeosu <ceh2805@gmail.com>
2024-04-11 22:47:58 +0900

    PostsService 테스트 모듈 생성 코드 추가

--- src/posts/posts.service.spec.ts
+++ src/posts/posts.service.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostsService } from './posts.service';
 
+// Test.createTestingModule() 메서드를 사용하여 PostsService를 테스트하는 모듈을 생성
 describe('PostsService', () => {
   let service: PostsService;
 
